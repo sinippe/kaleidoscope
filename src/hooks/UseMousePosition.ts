5b8cbe7947398bbfa9f11fe0f7b4318736c5d186
Philippe Ottenwaelter <pottenwaelter@free.fr>
2020-02-07 18:43:39 +0100

    Update UseMousePosition.ts

--- src/hooks/UseMousePosition.ts
+++ src/hooks/UseMousePosition.ts
@@ -1,50 +1,56 @@
-import {
-  useState,
-  useEffect,
-  useCallback,
-  Dispatch,
-  SetStateAction
-} from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 const useMousePosition = (props: {
-  useRate?: boolean;
-}): [{ x: number; y: number }, Dispatch<SetStateAction<boolean>>] => {
-  const useRate = props.useRate || false;
-  const [enabled, setEnabled] = useState(true);
+  useRate: boolean;
+  enabled?: boolean;
+}): [
+  { x: number; y: number },
+  React.Dispatch<React.SetStateAction<boolean>>
+] => {
+  const [enabled, setEnabled] = useState(props.enabled ? props.enabled : true);
 
-  const [position, setPosition] = useState({
+  const [mousePosition, setMousePosition] = useState<{
+    x: number;
+    y: number;
+  }>({
     x: 0,
     y: 0
   });
 
-  const onMouseMove = useCallback((event: Event) => {
-    const { x, y } = getMousePositionFromEvent(event);
-    if (useRate === true) {
-      setPosition({
-        x: x / window.innerWidth,
-        y: y / window.innerHeight
-      });
-    } else {
-      setPosition({
-        x,
-        y
-      });
-    }
-  }, []);
+  const onMouseMove = useCallback(
+    (event: Event) => {
+      const { x, y } = getMousePositionFromEvent(event);
+      if (props.useRate === true) {
+        setMousePosition({
+          x: x / window.innerWidth,
+          y: y / window.innerHeight
+        });
+      } else {
+        setMousePosition({
+          x,
+          y
+        });
+      }
+    },
+    [props.useRate]
+  );
 
   useEffect(() => {
+    window.removeEventListener("mousemove", onMouseMove);
+    window.removeEventListener("touchmove", onMouseMove);
     if (enabled) {
-      window.addEventListener("touchmove", onMouseMove);
       window.addEventListener("mousemove", onMouseMove);
+      window.addEventListener("touchmove", onMouseMove);
     }
 
     return () => {
-      window.removeEventListener("touchmove", onMouseMove);
       window.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("touchmove", onMouseMove);
     };
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [enabled]);
 
-  return [position, setEnabled];
+  return [mousePosition, setEnabled];
 };
 
 const getMousePositionFromEvent = (event: Event) => {
