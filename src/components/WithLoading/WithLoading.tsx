124da19d9edeafc605c876f36b0be4897763c3c7
Philippe Ottenwaelter <pottenwaelter@free.fr>
2020-02-11 15:44:11 +0100

    Add HOV WithLoading

---
+++ src/components/WithLoading/WithLoading.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { ProgressBar, Overlay, Classes } from "@blueprintjs/core";
+import styled from "styled-components";
+
+export type WithLoadingProps = {
+  loadingStart: () => void;
+  loadingComplete: () => void;
+};
+
+const withLoading = <P extends WithLoadingProps>(
+  Component: React.FC<P>
+): React.FC<Omit<P, keyof WithLoadingProps>> => props => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  let displayTimeout: number;
+
+  const loadingStart = () => {
+    clearTimeout(displayTimeout);
+    displayTimeout = setTimeout(() => {
+      setIsLoading(true);
+    }, 1000);
+  };
+
+  const loadingComplete = () => {
+    clearTimeout(displayTimeout);
+    setIsLoading(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Overlay className={Classes.OVERLAY_SCROLL_CONTAINER} isOpen={isLoading}>
+        <ProgressBarContainer>
+          <ProgressBar intent="primary" />
+        </ProgressBarContainer>
+      </Overlay>
+      <Component {...{ loadingStart, loadingComplete }} {...(props as P)} />;
+    </React.Fragment>
+  );
+};
+
+const ProgressBarContainer = styled.div`
+  left: 50vh;
+  top: 50vh;
+  margin: 0;
+  width: 300px;
+`;
+
+export default withLoading;
